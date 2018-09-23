3faab612849eca37c07eaef5d484fbbe5e8b12de
Jefferson Scher <jscher2000@gmail.com>
2018-09-22 18:41:49 -0700

    Fix Links
    So site can run from https://jscher2000.github.io/userchrome-dot-org/

--- uc.js
+++ uc.js
@@ -35,7 +35,7 @@ window.onload = function(e){
 	var homelink = document.querySelector('#logo:not([nolink])');
 	if (homelink){
 		homelink.style.cursor = 'pointer';
-		homelink.addEventListener('click', function(){window.location.href='https://www.userchrome.org/'});
+		homelink.addEventListener('click', function(){window.location.href='https://jscher2000.github.io/userchrome-dot-org/'});
 	}
 }
 function loadShare(anum, btn){
